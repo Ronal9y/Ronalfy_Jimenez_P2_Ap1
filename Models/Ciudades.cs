d4e12d2393e76fdf32d9aafb174730e0c638c977
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-10 18:52:46 -0400

    se agregaron requisitos en la tabla ciudades

--- Models/Ciudades.cs
+++ Models/Ciudades.cs
@@ -9,8 +9,9 @@ namespace Ronalfy_Jimenez_P2_Ap1.Models
         [Key]
         public int CiudadId { get; set; }
 
-        public string Nombre { get; set; }
-
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Este campo obligatorio")]
         public double Monto { get; set; }
 
     }
