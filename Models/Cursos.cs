d0b1547f522dade038a25fe52f21bfde9cf53b99
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-11 00:19:13 -0400

    corrigiendo errores

--- Models/Cursos.cs
+++ Models/Cursos.cs
@@ -15,7 +15,8 @@ namespace Ronalfy_Jimenez_P2_Ap1.Models
         [Required(ErrorMessage = "La asignatura es obligatoria")]
         [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
         public string? Asignatura { get; set; }
-
-        public virtual ICollection<CursosDetalle> Detalles { get; set; } = new List<CursosDetalle>();
+        public double Monto { get; set; }
+        public virtual ICollection<CursosDetalle> CursosDetalle { get; set; } = new List<CursosDetalle>();
+       
     }
 }
