fb21b4318e4b44399cb85889f6dcc7f24cddfbf4
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-11 00:19:33 -0400

    arreglando detalles

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,12 +6,16 @@ namespace Ronalfy_Jimenez_P2_Ap1.DAL
 {
     public class Contexto : DbContext
     {
+        internal object ciudades;
+
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
         }
 
         public DbSet<Ciudades> Ciudades { get; set; }
         public DbSet<Cursos> Cursos { get; set; }
+        public DbSet<CursosDetalle> CursosDetalle { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -27,16 +31,18 @@ namespace Ronalfy_Jimenez_P2_Ap1.DAL
 
             );
 
-            modelBuilder.Entity<Cursos>()
-        .HasMany(c => c.Detalles)
-        .WithOne(d => d.Cursos)
-        .OnDelete(DeleteBehavior.Cascade);
+      //      modelBuilder.Entity<Cursos>()
+      //.HasMany(c => c.CursosDetalle)
+      //.WithOne(d => d.Cursos)
+      //.HasForeignKey(d => d.CursosId)
+      //.OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<CursosDetalle>()
-        .HasOne(d => d.Ciudades)
-        .WithMany()
-        .HasForeignKey(d => d.CiudadId)
-        .OnDelete(DeleteBehavior.Restrict);
+
+        //    modelBuilder.Entity<CursosDetalle>()
+        //.HasOne(d => d.Ciudades)
+        //.WithMany()
+        //.HasForeignKey(d => d.CiudadId)
+        //.OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
